c139d520cc18d244f00d79f12d7c628c5ab7b750
Isa <146592315+heloisa-faria@users.noreply.github.com>
2026-04-20 22:56:10 -0300

    Multiplicacao

--- calculadora.py
+++ calculadora.py
@@ -4,7 +4,12 @@ def somar(a, b):
 
 print(f"Soma: {somar(10, 5)}")
 
-def dividi(a, b):
+def divisao(a, b):
     return a / b
 
-print(f"Divisão: {dividi(10, 5)}")
+print(f"Divisão: {divisao(10, 5)}")
+
+def multiplicacao(a, b):
+    return a * b
+
+print(f"Multiplicação: {multiplicacao(10, 5)}")
