d95f87410fa5db4a0610fba2325bd209c60eac7f
Isa <146592315+heloisa-faria@users.noreply.github.com>
2026-04-27 19:58:05 -0300

    Retirando os prints e arrumando a divisão por zero

--- projeto_calculadora/calculadora.py
+++ projeto_calculadora/calculadora.py
@@ -1,27 +1,21 @@
-# calculadora.py
+
 def somar(a, b):
     return a + b
 
-print(f"Soma: {somar(10, 5)}")
-
 def divisao(a, b):
+    if b == 0:
+        return "divisão por zero"
     return a / b
-
-print(f"Divisão: {divisao(10, 5)}")
 
 def multiplicacao(a, b):
     return a * b
 
-print(f"Multiplicação: {multiplicacao(10, 5)}")
-
 def subtracao(a, b):
     return a - b
 
-print(f"Subtração: {subtracao(10, 5)}")
-
 # Calculadora Finalizada para o projeto de DevOps
 def calculadora(a, b, operacao):
-    if operacao == "+": return a + b
-    if operacao == "/": return a / b
-    if operacao == "*": return a * b
-    if operacao == "-": return a - b
+    if operacao == "+": return somar(a,b)
+    if operacao == "/": return divisao(a, b)
+    if operacao == "*": return multiplicacao(a, b)
+    if operacao == "-": return subtracao(a, b)
